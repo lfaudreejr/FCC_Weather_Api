d86b6b897eaa626df7f4cca93636721c8f2d5811
Larry Faudree <lfaudrejr@hotmail.com>
2017-04-22 15:43:05 -0400

    added maxtemps

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ window.onload = (function()
       };
       xhttp.send();
     });
-  }
+  };
 
   (function getLocation()
   {
@@ -48,51 +48,50 @@ window.onload = (function()
     {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-        var location = get("https://api.apixu.com/v1/current.json?key=1391c71edd784b36b89143112172104&q=" + lat + "," + long)
-        .then(
-          function(location)
-          {
-          console.log(location);
-          city.textContent = location.location.name + ", " + location.location.region;
-          icon.setAttribute("src", "https://" + location.current.condition.icon);
-          p.innerHTML = location.current.temp_f + "&deg;F";
-          description.textContent = "'" + location.current.condition.text + "'";
-          },
-          function(err){
-            alert("Connection refused");
-          }
-        );
+      var location = get("https://api.apixu.com/v1/current.json?key=1391c71edd784b36b89143112172104&q=" + lat + "," + long)
+      .then(
+        function(location)
+        {
+        console.log(location);
+        city.textContent = location.location.name + ", " + location.location.region;
+        icon.setAttribute("src", "https://" + location.current.condition.icon);
+        p.innerHTML = location.current.temp_f + "&deg;F";
+        description.textContent = "'" + location.current.condition.text + "'";
+        },
+        function(err){
+          alert("Connection refused");
+        }
+      );
+
+      var forecast = get("https://api.apixu.com/v1/forecast.json?key=1391c71edd784b36b89143112172104&q=" + lat + "," + long + "&days=4")
+      .then(
+        function(forecast)
+        {
+          console.log(forecast);
+          var next = document.getElementById('next_day');
+          var next_icon = document.getElementById('next_icon');
+          var second_day = document.getElementById('second_day');
+          var second_icon = document.getElementById('second_icon');
+          var third_day = document.getElementById('third_day');
+          var third_icon = document.getElementById('third_icon');
+          next.innerHTML = "Tomorrow: " + forecast.forecast.forecastday[1].day.maxtemp_f + " &deg;F";
+          next_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[1].day.condition.icon);
+          second_day.innerHTML = "Day After: " + forecast.forecast.forecastday[2].day.maxtemp_f + " &deg;F";
+          second_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[2].day.condition.icon);
+          third_day.innerHTML = "Third day: " + forecast.forecast.forecastday[3].day.maxtemp_f + " &deg;F";
+          third_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[3].day.condition.icon);
+        },
+        function(err){
+          alert("Connection refused");
+        }
+      );
+    };
 
 
-        var forecast = get("https://api.apixu.com/v1/forecast.json?key=1391c71edd784b36b89143112172104&q=" + lat + "," + long + "&days=4")
-        .then(
-          function(forecast)
-          {
-            console.log(forecast);
-            var next = document.getElementById('next_day');
-            var next_icon = document.getElementById('next_icon');
-            var second_day = document.getElementById('second_day');
-            var second_icon = document.getElementById('second_icon');
-            var third_day = document.getElementById('third_day');
-            var third_icon = document.getElementById('third_icon');
-            next.innerHTML = "Tomorrow: " + forecast.forecast.forecastday[1].day.avgtemp_f + " &deg;F";
-            next_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[1].day.condition.icon);
-            second_day.innerHTML = "Day After: " + forecast.forecast.forecastday[2].day.avgtemp_f + " &deg;F";
-            second_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[2].day.condition.icon);
-            third_day.innerHTML = "Third day: " + forecast.forecast.forecastday[3].day.avgtemp_f + " &deg;F";
-            third_icon.setAttribute("src", "https://" + forecast.forecast.forecastday[3].day.condition.icon);
-          },
-          function(err){
-            alert("Connection refused");
-          }
-        );
+    function error(error)
+    {
+      alert("Unable to retrieve your location");
     }
 
-
-      function error(error)
-      {
-        alert("Unable to retrieve your location");
-      }
-
   })();
-})
+});
