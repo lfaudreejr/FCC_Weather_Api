c8607e178be0b7236570f097b51d76aaadd692a6
Larry Faudree <lfaudrejr@hotmail.com>
2017-08-11 23:05:15 -0400

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -23,86 +23,89 @@ window.onload = function() {
     var p = document.getElementById("temp");
     var city = document.getElementById("city");
     var description = document.getElementById("description");
+    var next = document.getElementById("next_day");
+    var next_icon = document.getElementById("next_icon");
+    var second_day = document.getElementById("second_day");
+    var second_icon = document.getElementById("second_icon");
+    var third_day = document.getElementById("third_day");
+    var third_icon = document.getElementById("third_icon");
     var lat;
     var long;
-
-    if (!navigator.geolocation) {
-      alert("Geolocation not supported by this browser");
-      return;
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error);
+    var weatherObj = new Object();
 
     function success(position) {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-      var location = get(
-        "https://api.apixu.com/v1/current.json?key=key&q=" + lat + "," + long
-      ).then(
-        function(location) {
-          console.log(location);
-          city.textContent =
-            location.location.name + ", " + location.location.region;
-          icon.setAttribute(
-            "src",
-            "https://" + location.current.condition.icon
-          );
-          p.innerHTML = location.current.temp_f + "&deg;F";
-          description.textContent = "'" + location.current.condition.text + "'";
-        },
-        function(err) {
-          alert("Connection refused");
-        }
-      );
-
-      var forecast = get(
-        "https://api.apixu.com/v1/forecast.json?key=key&q=" +
-          lat +
-          "," +
-          long +
-          "&days=4"
-      ).then(
-        function(forecast) {
-          console.log(forecast);
-          var next = document.getElementById("next_day");
-          var next_icon = document.getElementById("next_icon");
-          var second_day = document.getElementById("second_day");
-          var second_icon = document.getElementById("second_icon");
-          var third_day = document.getElementById("third_day");
-          var third_icon = document.getElementById("third_icon");
-          next.innerHTML =
-            "Tomorrow: " +
-            forecast.forecast.forecastday[1].day.maxtemp_f +
-            " &deg;F";
-          next_icon.setAttribute(
-            "src",
-            "https://" + forecast.forecast.forecastday[1].day.condition.icon
-          );
-          second_day.innerHTML =
-            "Day After: " +
-            forecast.forecast.forecastday[2].day.maxtemp_f +
-            " &deg;F";
-          second_icon.setAttribute(
-            "src",
-            "https://" + forecast.forecast.forecastday[2].day.condition.icon
-          );
-          third_day.innerHTML =
-            "Third day: " +
-            forecast.forecast.forecastday[3].day.maxtemp_f +
-            " &deg;F";
-          third_icon.setAttribute(
-            "src",
-            "https://" + forecast.forecast.forecastday[3].day.condition.icon
-          );
-        },
-        function(err) {
-          alert("Connection refused");
-        }
-      );
+      makeWeatherObj();
     }
 
     function error(error) {
       alert("Unable to retrieve your location");
+    }
+
+    var getCurrentWeather = function getCurrentWeather() {
+      return get(
+        "https://api.apixu.com/v1/current.json?key=1391c71edd784b36b89143112172104&q=" +
+          lat +
+          "," +
+          long
+      );
+    };
+
+    var getForecast = function getForecast() {
+      return get(
+        "https://api.apixu.com/v1/forecast.json?key=1391c71edd784b36b89143112172104&q=" +
+          lat +
+          "," +
+          long +
+          "&days=4"
+      );
+    };
+
+    function makeWeatherObj() {
+      Promise.resolve(getForecast()).then(function(results) {
+        weatherObj.current = results.current;
+        weatherObj.forecast = results.forecast.forecastday;
+        weatherObj.location = results.location;
+        console.log("Weather: ", weatherObj);
+        setCurrentWeather();
+        setForecast();
+      });
+    }
+
+    function setCurrentWeather() {
+      city.textContent =
+        weatherObj.location.name + ", " + weatherObj.location.region;
+      icon.setAttribute("src", "https://" + weatherObj.current.condition.icon);
+      p.innerHTML = weatherObj.current.temp_f + "&deg;F";
+      description.textContent = "'" + weatherObj.current.condition.text + "'";
+    }
+
+    function setForecast() {
+      next.innerHTML =
+        "Tomorrow: " + weatherObj.forecast[1].day.maxtemp_f + " &deg;F";
+      next_icon.setAttribute(
+        "src",
+        "https://" + weatherObj.forecast[1].day.condition.icon
+      );
+      second_day.innerHTML =
+        "Day After: " + weatherObj.forecast[2].day.maxtemp_f + " &deg;F";
+      second_icon.setAttribute(
+        "src",
+        "https://" + weatherObj.forecast[2].day.condition.icon
+      );
+      third_day.innerHTML =
+        "Third day: " + weatherObj.forecast[3].day.maxtemp_f + " &deg;F";
+      third_icon.setAttribute(
+        "src",
+        "https://" + weatherObj.forecast[3].day.condition.icon
+      );
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+    if (!navigator.geolocation) {
+      alert("Geolocation not supported by this browser");
+      return;
     }
   })();
 };
